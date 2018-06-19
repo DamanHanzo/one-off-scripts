ee7bf5d1ebfe231b2a3015db424ae5af0fb2f7ef
DamanHanzo <singhdamanpreet1234@gmail.com>
2018-06-19 14:16:00 -0400

    added remote branch checkout functionality

--- bash_shell/diffs.sh
+++ bash_shell/diffs.sh
@@ -1,49 +1,71 @@
 #! /bin/bash
-echo -n "Please enter directory path of the repository(leave empty if in repo dir): "
+set -e
+if [ -e "diffsraw.diff" ]; then
+	echo "" > diffsraw.diff #clear out the diffsraw file if it exists
+fi
+RED='\033[0;31m'
+GREEN='\033[1;32m' 
+NC='\033[0m'
+echo -ne "Please enter directory path of the repository(${RED}leave empty if already in repo dir${NC}): "
 read directory
 if [ -n "$directory" ]; then 
 	cd $directory
 else 
 	printf "Working Directory: $(pwd)\n"
 fi
-echo -n "Please enter current branch: "
-read current
-echo -n "Please enter the branch to compare: "
-read compare
+echo -n "Please enter branch a: "
+read brancha
+echo -n "Please enter the branch b: "
+read branchb
+echo -ne "${GREEN}Sacred DDL${NC} source(default: master): "
+read ddlsource
+if [ -z "$ddlsource" ]; then
+	ddlsource="master" #default to DataGuard if ddl source is isn't specified
+	git checkout "$ddlsource" >/dev/null 2>&1
+else
+	localbranch=$(git branch | grep -w "$ddlsource" | tr -d '[:space:]')
+	if [ -n $localbranch ]; then 
+		git checkout "$ddlsource" >/dev/null 2>&1
+	elif [ -z $localbranch ]; then
+		remotebranch=$(git ls-remote --heads origin "$ddlsource" | grep "$ddlsource" | tr -d '[:space:]')
+		ddlsourcefromremote="origin/$ddlsource"
+		if [ -n $remotebranch ]; then 
+			git checkout --track "$ddlsourcefromremote" >/dev/null 2>&1
+		else
+			printf "${RED}The sacred ddl source branch doesn't exist.${NC}/n"
+		fi
+	fi
+fi
 { 
-	git diff $current..$compare >/dev/null 2>&1
+	git diff $brancha..$branchb >/dev/null 2>&1
 } || {
-	printf "Please make sure that the branches you want to compare are checked out locally.\n"
-	set -e
+	printf "${RED}Please make sure that the branches you want to compare are checked out locally.${NC}\n"
 }
-totalfilesmodified=($(git diff $current..$compare --diff-filter=r --numstat | grep -Ev '^1\s+1[^1]' | awk '{print $3}'))
+totalfilesmodified=($(git diff "$brancha".."$branchb" --diff-filter=r --numstat | grep -Ev '^1\s+1[^1]' | awk '{print $3}'))
 if [ "${#totalfilesmodified[@]}" -gt 0 ]; then 
 	printf "Date ran: $(date +%m-%d-%y)\n" >> diffsraw.diff
 	printf "Found changes in ${#totalfilesmodified[@]} files\n"
-	if [ "$(git branch | grep -E '^[*]' | cut -d* -f2 | tr -d '[:space:]')" != "master" ]; then 
-		git checkout master
-	fi
 	for i in "${totalfilesmodified[@]}"; do
+		object=$(echo "$i" | cut -d/ -f1)
 		printf "Evaluating changes in $i\n"
 		printf "Diffs in $i" >> diffsraw.diff
-		diff=$(git diff $current $compare -- $i)
+		diff=$(git diff "$brancha" "$branchb" -- "$i")
 		echo -En "$diff" >> diffsraw.diff
 		printf "\nend of diffs in $i\n" >> diffsraw.diff
-		#TODO: add logic to make sure that the tables are excluded
-		if [ $(git diff $current $compare -- $i | wc -l) -ge 2 ]; then 
+		if [ $(git diff "$brancha" "$branchb" -- "$i" | wc -l) -ge 2 ] && [ "$object" != "TABLE" ]; then
 			if [ -e "$i" ]; then
-				printf "File found in master\n"
-				printf "\nComplete DDL from DataGuard(Prod)\n\n" >> diffsraw.diff
+				printf "\nComplete DDL from DataGuard(Prod):\n\n" >> diffsraw.diff
 				cat "./$i" >> diffsraw.diff
-				printf "\nDDL END FOR $i\n">> diffsraw.diff
-			else 
-				printf "$i not found in master\n"
-		 		printf "\n$i not found in master\n" >> diffsraw.diff
+				printf "\nDDL END FOR $i\n\n">> diffsraw.diff
+			else
+				printf "${RED}$i not found in $ddlsource${NC}\n"
+		 		printf "$i not found in $ddlsource\n\n" >> diffsraw.diff
 	 		fi
+ 		# handle table ddl parsing logic here
+	 	# else 
 		fi
-		# echo -En "$diffcleaned">> diffadditions.diff
 	done
 else 
-	printf "No changes found. Both branches are upto date.\n"
+	printf "${GREEN}No changes found. Both branches are upto date.${NC}\n"
 fi
 read -p "Press any key to exit..."
